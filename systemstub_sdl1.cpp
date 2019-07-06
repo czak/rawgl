08aee4efc6dfbf32d319a69b389f0ceff3024f25
Łukasz Adamczak <lukasz@czak.pl>
2019-07-06 16:28:22 +0200

    Basic SDL1 initialization & input handling

--- systemstub_sdl1.cpp
+++ systemstub_sdl1.cpp
@@ -20,6 +20,8 @@ struct SystemStub_SDL1 : SystemStub {
 	int _texW, _texH;
 	int _screenshot;
 
+	SDL_Surface *_screen;
+
 	SystemStub_SDL1();
 	virtual ~SystemStub_SDL1() {}
 
@@ -40,13 +42,16 @@ struct SystemStub_SDL1 : SystemStub {
 const float SystemStub_SDL1::kAspectRatio = 16.f / 10.f;
 
 SystemStub_SDL1::SystemStub_SDL1()
-	: _w(0), _h(0), _texW(0), _texH(0) {
+	: _w(0), _h(0), _texW(0), _texH(0), _screen(0) {
 }
 
 void SystemStub_SDL1::init(const char *title, const DisplayMode *dm) {
+	SDL_Init(SDL_INIT_VIDEO);
+	_screen = SDL_SetVideoMode(320, 200, 32, 0);
 }
 
 void SystemStub_SDL1::fini() {
+	SDL_Quit();
 }
 
 void SystemStub_SDL1::prepareScreen(int &w, int &h, float ar[4]) {
@@ -59,6 +64,82 @@ void SystemStub_SDL1::setScreenPixels565(const uint16_t *data, int w, int h) {
 }
 
 void SystemStub_SDL1::processEvents() {
+	SDL_Event ev;
+	while(SDL_PollEvent(&ev)) {
+		switch (ev.type) {
+		case SDL_QUIT:
+			_pi.quit = true;
+			break;
+		case SDL_KEYUP:
+			switch(ev.key.keysym.sym) {
+			case SDLK_LEFT:
+				_pi.dirMask &= ~PlayerInput::DIR_LEFT;
+				break;
+			case SDLK_RIGHT:
+				_pi.dirMask &= ~PlayerInput::DIR_RIGHT;
+				break;
+			case SDLK_UP:
+				_pi.dirMask &= ~PlayerInput::DIR_UP;
+				break;
+			case SDLK_DOWN:
+				_pi.dirMask &= ~PlayerInput::DIR_DOWN;
+				break;
+			case SDLK_SPACE:
+			case SDLK_RETURN:
+				_pi.button = false;
+				break;
+			case SDLK_s:
+				_pi.screenshot = true;
+				break;
+			case SDLK_c:
+				_pi.code = true;
+				break;
+			case SDLK_p:
+				_pi.pause = true;
+				break;
+			default:
+				break;
+			}
+			break;
+		case SDL_KEYDOWN:
+			if (ev.key.keysym.mod & KMOD_ALT) {
+				if (ev.key.keysym.sym == SDLK_RETURN || ev.key.keysym.sym == SDLK_KP_ENTER) {
+				} else if (ev.key.keysym.sym == SDLK_x) {
+					_pi.quit = true;
+				}
+				break;
+			} else if (ev.key.keysym.mod & KMOD_CTRL) {
+				if (ev.key.keysym.sym == SDLK_f) {
+					_pi.fastMode = true;
+				}
+				break;
+			}
+			_pi.lastChar = ev.key.keysym.sym;
+			switch(ev.key.keysym.sym) {
+			case SDLK_LEFT:
+				_pi.dirMask |= PlayerInput::DIR_LEFT;
+				break;
+			case SDLK_RIGHT:
+				_pi.dirMask |= PlayerInput::DIR_RIGHT;
+				break;
+			case SDLK_UP:
+				_pi.dirMask |= PlayerInput::DIR_UP;
+				break;
+			case SDLK_DOWN:
+				_pi.dirMask |= PlayerInput::DIR_DOWN;
+				break;
+			case SDLK_SPACE:
+			case SDLK_RETURN:
+				_pi.button = true;
+				break;
+			default:
+				break;
+			}
+			break;
+		default:
+			break;
+		}
+	}
 }
 
 void SystemStub_SDL1::sleep(uint32_t duration) {
